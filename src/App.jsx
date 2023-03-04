20f9dd12fe8ac16db4dd03e2183b2d42cc0f19f7
seangaaab <seangabbayron@gmail.com>
2023-03-04 15:10:47 +0800

    Processing of results form Firestore

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ import "./App.css";
 // Just make sure to change it in the App.jsx file as well.
 
 function App() {
-	const [todos, setTodos] = useState([]);
+	const [pokemons, setPokemons] = useState([]);
 	const [input, setInput] = useState("");
 
-	async function addTodo(event) {
+	async function addPokemon(event) {
 		event.preventDefault();
 
 		// ... code goes here
@@ -33,22 +33,24 @@ function App() {
 		// ... code goes here
 	}
 
-	async function deleteTodo(id) {
+	async function deletePokemon(id) {
 		// ... code goes here
 	}
 
 	useEffect(() => {
-		const sortedTodos = query(
-			collection(db, "todos"),
+		const sortedPokemons = query(
+			collection(db, "pokemons"),
 			orderBy("timestamp", "desc")
 		);
 
-		const unsubscribe = onSnapshot(sortedTodos, (snapshot) => {
-			setTodos(
+		const unsubscribe = onSnapshot(sortedPokemons, (snapshot) => {
+			setPokemons(
 				snapshot.docs.map((doc) => ({
 					id: doc.id,
-					todo: doc.data().todo,
-					completed: doc.data().completed,
+					name: doc.data().name,
+					votes: doc.data().votes,
+					voted_againsts: doc.data().voted_againsts,
+					score: doc.data() - doc.data().voted_againsts
 				}))
 			);
 		});
@@ -63,7 +65,7 @@ function App() {
 	<section className="container">
 		<h1>Pokerater</h1>
 		<div>
-			<form onSubmit={addTodo}>
+			<form onSubmit={addPokemon}>
 				<img src="https://pokeapi.co/api/v2/pokemon/1/sprites/front_default" />
 				<h3>Name: </h3>
 				<h3>Rank: </h3>
@@ -73,6 +75,19 @@ function App() {
 					<button>Down Vote</button>
 				</span>
 			</form>
+		</div>
+		<div>
+			{
+				pokemons.map((pokemon) => (
+					<div key={pokemon.id}>
+						<h3>{pokemon.name}</h3>
+						<p>{pokemon.votes}</p>
+						<p>{pokemon.voted_againsts}</p>
+						<p>{pokemon.votes} </p>
+						<p>{pokemon.score}</p>
+					</div>
+				))
+			}
 		</div>
 	</section>
 	);
